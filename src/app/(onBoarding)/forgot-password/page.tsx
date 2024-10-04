6822fb33c002afeb448e3973be16a5b40c684772
John Vilela <johnvvilela@gmail.com>
2024-10-03 23:39:04 -0300

    feat: create forgot-password page layout

--- src/app/(onBoarding)/forgot-password/page.tsx
+++ src/app/(onBoarding)/forgot-password/page.tsx
@@ -1,15 +1,19 @@
-import { Card, CardHeader } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 export default function ForgotPasswordPage() {
   return (
-    <main className="h-screen grid place-items-center">
-      <Card>
-        <CardHeader>
-          <CardHeader className="text-4xl font-bold">
-            Forgot your password?
-          </CardHeader>
-        </CardHeader>
-      </Card>
-    </main>
+    <>
+      <h1 className="text-4xl font-bold mb-4">Forgot your password?</h1>
+      <p>
+        {
+          "Enter your email and we'll send the instructions to reset your password"
+        }
+      </p>
+      <form className="flex flex-col gap-2 mt-8">
+        <Input label="Email" type="email" />
+        <Button type="submit">Send instructions</Button>
+      </form>
+    </>
   );
 }
