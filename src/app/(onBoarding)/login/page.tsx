f6597531bb84ad8988259c2c1b01a7686e3901a7
John Vilela <johnvvilela@gmail.com>
2024-10-03 23:35:16 -0300

    feat: create login page layout

--- src/app/(onBoarding)/login/page.tsx
+++ src/app/(onBoarding)/login/page.tsx
@@ -1,13 +1,27 @@
-import { Card, CardHeader } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
 
 export default function LoginPage() {
   return (
-    <main className="h-screen grid place-items-center">
-      <Card>
-        <CardHeader>
-          <CardHeader className="text-4xl font-bold">Welcome back!</CardHeader>
-        </CardHeader>
-      </Card>
-    </main>
+    <>
+      <h1 className="text-4xl font-bold mb-2">Welcome back!</h1>
+      <p>Enter your credentials to login</p>
+      <form className="flex flex-col gap-2 mt-8">
+        <Input label="Email" type="email" />
+        <Input label="Password" type="password" />
+        <Button
+          variant="link"
+          className="text-xs p-0 h-auto justify-end mb-4"
+          asChild
+        >
+          <Link href={'/forgot-password'}>Forgot password?</Link>
+        </Button>
+        <Button type="submit">Login</Button>
+        <Button variant="ghost" asChild>
+          <Link href={'/register'}>{"Don't have an account? Register"}</Link>
+        </Button>
+      </form>
+    </>
   );
 }
